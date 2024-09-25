d37ab6ad4604eb20d63b601f1042edc7bd5f9de7
Diego Maldonado <dj_maldonado19@hotmail.es>
2024-09-25 07:22:02 -0600

    Añadiendo ProductoDAO para manejar las consultas a productos

---
+++ src/main/java/dmaldonado/gamerproxela/controller/ProductoDAO.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dmaldonado.gamerproxela.controller;
+
+import dmaldonado.gamerproxela.domain.Producto;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ *
+ * @author DiegoMaldonado
+ */
+public class ProductoDAO {
+
+    private Connection conexion;
+
+    public ProductoDAO(Connection conexion) {
+        this.conexion = conexion;
+    }
+
+    public Optional<List<Producto>> obtenerProductos() throws SQLException {
+        List<Producto> productos = new ArrayList<>();
+        String sql = "SELECT * FROM cp.producto";
+
+        try (PreparedStatement pstmt = conexion.prepareStatement(sql)) {
+            ResultSet rs = pstmt.executeQuery();
+
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String nombre = rs.getString("nombre");
+                String descripcion = rs.getString("descripcion");
+                double precio = rs.getDouble("precio");
+
+                Producto producto = new Producto(id, nombre, descripcion, precio);
+                productos.add(producto);
+            }
+        }
+
+        return productos.isEmpty() ? Optional.empty() : Optional.of(productos);
+    }
+    
+    public void insertarProducto(Producto producto) throws SQLException{
+        String sqlProducto = "INSERT INTO cp.producto (nombre, descripcion, precio) VALUES (?, ?, ?)";
+        PreparedStatement pstmtProducto = conexion.prepareStatement(sqlProducto);
+        
+        pstmtProducto.setString(1, producto.getNombre());
+        pstmtProducto.setString(2, producto.getDescripcion());
+        pstmtProducto.setDouble(3, producto.getPrecio());
+        
+        pstmtProducto.executeUpdate();
+        
+        pstmtProducto.close();
+    }
+}
